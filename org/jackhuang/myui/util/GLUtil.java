6fde3b6542747055cdd97133d67c70de31be17e6
huanghongxun <huanghongxun2008@126.com>
2014-07-21 18:40:10 +0800

    commit5

--- org/jackhuang/myui/util/GLUtil.java
+++ org/jackhuang/myui/util/GLUtil.java
@@ -1,7 +1,9 @@
 /**
- * MyUI Mod is under MIT License.
- * READ THE LICENSE FIRST.
- * @author huangyuhui
+ * Copyright (c) Huang Yuhui, 2014
+ * 
+ * "MyUI" is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * http://www.mod-buildcraft.com/MMPL-1.0.txt
  */
 package org.jackhuang.myui.util;
 
